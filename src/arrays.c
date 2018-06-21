4fa7e96a1f81177213e654cc28f835dae23915fc
ArroWinter <pavlikozzchannel@gmail.com>
2018-06-21 18:48:05 +0700

    Little fix of array functions and add Bomber function

--- src/arrays.c
+++ src/arrays.c
@@ -4,7 +4,7 @@
 using namespace std;
 
 void Create_Mass(int **a, int n){
-    for (int i=0; i<n; i++){
+    for (int i=0; i<n+2; i++){
         for(int j=0; j<n; j++){
             a[i][j]=0;
         }
@@ -12,10 +12,19 @@ void Create_Mass(int **a, int n){
 }
 
 void Create_Mass(char **b, int n){
-    for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
+    for (int i=0; i<n+2; i++){
+        for (int j=0; j<n+2; j++){
             b[i][j]=' ';
         }
     }
     b[0][0]='X';
+}
+
+void Bomber(int **a, int n){
+    int x,y;
+    for (int i=0; i<n; i++){
+        x=rand()%(n-1) +1;
+        y=rand()%(n-1) +1;
+        if (a[x][y]!=9) a[x][y]=9; else i--;
+    }
 }
