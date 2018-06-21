0149a6b40739889f8ec216818d41e3ca808336df
ArroWinter <pavlikozzchannel@gmail.com>
2018-06-21 20:08:33 +0700

    Delete conio.h because this is linux and add arrays memory

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <iomanip>
 #include <time.h>
-#include <conio.h>
 
 using namespace std;
 
@@ -9,6 +8,15 @@ int **a;
 char **b;
 
 int main(){
+    
     setlocate(LC_ALL, "Russian");
+    srand(time(NULL));
+    a = new int *[100];
+    for (int i=0; i<101; i ++)
+        a[i] = new int [100];
+    b = new char *[100];
+    for (int i=0; i<101; i ++)
+        b[i] = new char [100];
+    
     return 0;
 }
