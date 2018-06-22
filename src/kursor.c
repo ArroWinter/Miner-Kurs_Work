790042076a5133528c4c344ddc1f51a800968389
ArroWinter <pavlikozzchannel@gmail.com>
2018-06-22 15:45:52 +0700

    Update kursor move

--- src/kursor.c
+++ src/kursor.c
@@ -1,7 +1,6 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include "kursor.h"
-
-using namespace std;
 
 void Kursor(int **a, char **b, int p, char &temp){
     int z, c;
@@ -14,69 +13,23 @@ void Kursor(int **a, char **b, int p, char &temp){
             }
         }
     }
-    cin >> n;
-    if (n=='d'){
-        if (c+1<p+1){
-            b[z][c]=temp; 
-            temp=b[z][c+1];
-            b[z][c+1]='X';
-        }
-    }
-    else Kursor(a, b, p, temp);
-    if (n=='a'){
-        if (c-1>0){
-            b[z][c]=temp; 
-            temp=b[z][c-1]; 
-            b[z][c-1]='X';
-        }
-    }
-    else Kursor(a, b, p, temp);        
-    if (n=='w'){
-        if (z-1>0){
-            b[z][c]=temp; 
-            temp=b[z-1][c]; 
-            b[z-1][c]='X';
-        }
-    }
-    else Kursor(a, b, p, temp);
-    if (n=='s'){
-        if (z+1<p+1){
-            b[z][c]=temp;
-            temp=b[z+1][c]; 
-            b[z+1][c]='X';
-        }
-    }
-    else Kursor(a, b, p, temp);
-    if (n=='f'){
-        if (a[z][c]==0){
-            temp='0';
-        }
-        if (a[z][c]==1){
-            temp='1';
-        }
-        if (a[z][c]==2){
-            temp='2';
-        }
-        if (a[z][c]==3){
-            temp='3';
-        }
-        if (a[z][c]==4){
-            temp='4';
-        }
-        if (a[z][c]==5){
-            temp='5';
-        }
-        if (a[z][c]==6){
-            temp='6';
-        }
-        if (a[z][c]==7){
-            temp='7';
-        }
-        if (a[z][c]==8){
-            temp='8';
-        }
-        if (a[z][c]==9){
-            temp='9';
-        }
+    scanf("%c", &n);
+    switch(n){
+        case 'd' : if (c+1<p+1){ b[z][c]=temp; temp=b[z][c+1]; b[z][c+1]='X';} else Kursor(a, b, p, temp); break;
+        case 'a' : if (c-1>0){ b[z][c]=temp; temp=b[z][c-1]; b[z][c-1]='X';} else Kursor(a, b, p, temp); break;
+        case 'w' : if (z-1>0){ b[z][c]=temp; temp=b[z-1][c]; b[z-1][c]='X';} else Kursor(a, b, p, temp); break;
+        case 's' : if (z+1<p+1){ b[z][c]=temp; temp=b[z+1][c]; b[z+1][c]='X';} else Kursor(a, b, p, temp, flags, end); break;
+        case 'f' : switch(a[z][c]){
+                       case 0 : temp='0'; break;
+                       case 1 : temp='1'; break;
+                       case 2 : temp='2'; break;
+                       case 3 : temp='3'; break;
+                       case 4 : temp='4'; break;
+                       case 5 : temp='5'; break;
+                       case 6 : temp='6'; break;
+                       case 7 : temp='7'; break;
+                       case 8 : temp='8'; break;
+                       case 9 : temp='9'; break;
+                   } break;
     }
 }
