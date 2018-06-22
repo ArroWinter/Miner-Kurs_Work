d30357ac744b56f23640f005638ab77f060750f8
ArroWinter <pavlikozzchannel@gmail.com>
2018-06-22 13:01:55 +0700

    Update kursor function

--- src/kursor.c
+++ src/kursor.c
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void Kursor(int **a, char **b, int p){
+void Kursor(int **a, char **b, int p, char &temp){
     int z, c;
     char n;
     for (int i=0; i<=p; i++){
@@ -17,18 +17,34 @@ void Kursor(int **a, char **b, int p){
     cin >> n;
     if (n=='d'){
         if (c+1<p+1){
+            b[z][c]=temp; 
+            temp=b[z][c+1];
+            b[z][c+1]='X';
         }
     }
+    else Kursor(a, b, p, temp);
     if (n=='a'){
         if (c-1>0){
+            b[z][c]=temp; 
+            temp=b[z][c-1]; 
+            b[z][c-1]='X';
         }
-    }        
+    }
+    else Kursor(a, b, p, temp);        
     if (n=='w'){
         if (z-1>0){
+            b[z][c]=temp; 
+            temp=b[z-1][c]; 
+            b[z-1][c]='X';
         }
     }
+    else Kursor(a, b, p, temp);
     if (n=='s'){
         if (z+1<p+1){
+            b[z][c]=temp;
+            temp=b[z+1][c]; 
+            b[z+1][c]='X';
         }
     }
+    else Kursor(a, b, p, temp);
 }
