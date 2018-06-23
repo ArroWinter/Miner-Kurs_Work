45d364b6f93fd43090e002e63b8f889414347169
ArroWinter <pavlikozzchannel@gmail.com>
2018-06-23 16:44:13 +0700

    Edit includes and add function

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
-#include <iostream>
-#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-using namespace std;
-
-int **a;
+int **a, end;
 char **b;
 
 int main(){
-    
-    setlocate(LC_ALL, "Russian");
     srand(time(NULL));
     a = new int *[100];
     for (int i=0; i<101; i ++)
@@ -17,6 +13,6 @@ int main(){
     b = new char *[100];
     for (int i=0; i<101; i ++)
         b[i] = new char [100];
-    
+    Starter();
     return 0;
 }
