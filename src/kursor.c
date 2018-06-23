a6a3520207357048eeffe7123ab8c36ca2f16860
ArroWinter <pavlikozzchannel@gmail.com>
2018-06-23 16:33:50 +0700

    Add check flags function and update kursor function

--- src/kursor.c
+++ src/kursor.c
@@ -68,7 +68,23 @@ void Zero(int **a, char **b, int z, int c){
     if ((a[z][c-1]==4)&&(b[z][c-1]!='P')) b[z][c-1]='4';
 }
 
-void Kursor(int **a, char **b, int p, char &temp, int &flags){
+int Check_Flags(int **a, char **b, int z, int c, int p, char temp, int flags){
+    int w;
+    char e;
+    e=b[z][c];
+    b[z][c]=temp;
+
+    for (int i=1; i<p+1; i++){
+        for (int j=1; j<p+1; j++){
+            if ((b[i][j]=='P')&&(a[i][j]==9))
+                w++;
+        }
+    }
+    b[z][c]=e;
+    return w;
+}
+
+void Kursor(int **a, char **b, int p, char &temp, int &flags, int &end){
     int z, c;
     char n;
     for (int i=0; i<=p; i++){
@@ -85,7 +101,7 @@ void Kursor(int **a, char **b, int p, char &temp, int &flags){
         case 'a' : if (c-1>0){ b[z][c]=temp; temp=b[z][c-1]; b[z][c-1]='X';} else Kursor(a, b, p, temp); break;
         case 'w' : if (z-1>0){ b[z][c]=temp; temp=b[z-1][c]; b[z-1][c]='X';} else Kursor(a, b, p, temp); break;
         case 's' : if (z+1<p+1){ b[z][c]=temp; temp=b[z+1][c]; b[z+1][c]='X';} else Kursor(a, b, p, temp, flags, end); break;
-        case 'e' : if (temp=='P') { temp=' '; flags++;} else { if (flags>0){ temp='P'; flags--;} else Kursor(a, b, p, temp, flags, end);} break;
+        case 'e' : if (temp=='P') { temp=' '; flags++;} else { if (flags>0){ temp='P'; flags--;} else Kursor(a, b, p, temp, flags, end);} end=Check_Flags(a, b, z, c, p, temp, flags); break;
         case 'q' : Print_Keys(); break;
         case 'f' : switch(a[z][c]){
                        case 0 : temp='0'; Zero(a, b, z, c); break;
