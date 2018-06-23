3d09dce8d1294daa0dbac6c985925f7b474b1df4
ArroWinter <pavlikozzchannel@gmail.com>
2018-06-23 18:32:53 +0700

    Fix errors

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,8 @@
 #include <time.h>
 #include "starter.h"
 
-int **a, end;
-char **b;
-
 int main(){
     srand(time(NULL));
-    a = new int *[100];
-    for (int i=0; i<101; i ++)
-        a[i] = new int [100];
-    b = new char *[100];
-    for (int i=0; i<101; i ++)
-        b[i] = new char [100];
     Starter();
     return 0;
 }
