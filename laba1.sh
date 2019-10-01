4abf54743523f932566762d4e91e591cbad489f2
Приходько Павел Алексеевич <ip712s20@lwp04.csc.local>
2019-10-01 13:32:38 +0700

    ABC Lab 1

--- laba1.sh
+++ laba1.sh
@@ -7,8 +7,8 @@ echo -n "Доменное имя ПК: "
 hostname
 
 echo "Процессор:"
-echo -e -n "\t* Модель - " ; lscpu|grep Модель:| awk '{print $NF}'; 
-echo -e -n "\t* Архитектура - " ; lscpu|grep Архитектура:| awk '{print $NF}'; 
+echo -e -n "\t* Модель - " ; lscpu|grep Модель | awk '{print $NF}'; 
+echo -e -n "\t* Архитектура - " ; lscpu|grep Архитектура| awk '{print $NF}'; 
 echo -e -n "\t* Тактовая частота - " ; lscpu|grep Имя| awk '{print $NF}'; 
 echo -e -n "\t* Количество ядер - " ; lscpu|grep Ядер| awk '{print $NF}'; 
 echo -e -n "\t* Количество потоков на одно ядро - " ; lscpu|grep Thread| awk '{print $NF}'; 
@@ -18,8 +18,20 @@ echo -e -n "\t* Всего - " ; free -h|grep Mem| awk '{print $2}';
 echo -e -n "\t* Доступно - " ; free -h|grep Mem| awk '{print $NF}'; 
 
 echo "Жесткий диск:"
-df
-#echo -e -n "\t* Всего - " ; df |grep Mem| awk '{print $2}';
-#echo -e -n "\t* Доступно - " ; df |grep Mem| awk '{print $NF}'; 
+
+
+echo -e -n "\t* Всего - " ; df | awk '{sum +=$2} END {print sum}';
+echo -e -n "\t* Доступно - " ; lsblk | awk '{sum +=$4} END {print sum}'; 
+echo -e -n "\t* Смонтировано в корневую директорию - /" ; df -h|grep "6% /"| cut -c 2-10;
+echo -e -n "\t* SWAP всего - " ; free -h|grep Swap| awk '{print $2}';
+echo -e -n "\t* SWAP доступно - " ; free -h|grep Swap| awk '{print $2}';
+
+echo -e "Имя сетевого интерфейса\t\tMAC адрес\t\tIP адрес\t\tСкорость соединения"
+for iface in /sys/class/net/*; do
+	echo -ne "${iface##*/}\t\t\t\t";
+	tr "\n" "\t" < "$iface/address";
+	printf "%-24s" " $(ip -4 -o a | grep " ${iface##*/} " | tr -s ' ' |  cut -d ' ' -f4 | tr -d '\n')"; 
+	ip -4 -0 a | grep " ${iface##*/}: " | tr -s ' ' | cut -d' ' -f13  
+done
 
 
